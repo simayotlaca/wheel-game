84c017c1d9e26f27557c543d11647ce5cd2e045c
Simay Otlaca <simay.otlaca@sabanciuniv.edu>
2026-06-05 17:03:29 +0300

    cleaning

--- Assets/Scripts/Animation/RewardFeedbackController.cs
+++ Assets/Scripts/Animation/RewardFeedbackController.cs
@@ -20,11 +20,9 @@ public class RewardFeedbackController : MonoBehaviour
 
     private FeedbackState state = FeedbackState.Disabled;
     private RewardAnimationSequence animation_sequence;
-    private RunEventPass event_pass;
 
     void Awake()
     {
-        event_pass = new RunEventPass(controller.Events);
         animation_sequence = CreateAnimationSequence();
     }
 
@@ -42,35 +40,12 @@ public class RewardFeedbackController : MonoBehaviour
     void OnEnable()
     {
         state = FeedbackState.Listening;
-        event_pass.Subscribe<RunPendingClearedEvent>(HandlePendingCleared);
-        event_pass.Subscribe<RunDeathHitEvent>(HandleDeathHit);
     }
 
     void OnDisable()
     {
-        event_pass.ReleaseAll();
         state = FeedbackState.Disabled;
         animation_sequence.Cancel();
-    }
-
-    private void HandlePendingCleared(RunPendingClearedEvent _)
-    {
-        EnterCleanupState();
-    }
-
-    private void HandleDeathHit(RunDeathHitEvent _)
-    {
-        EnterCleanupState();
-    }
-
-    private void EnterCleanupState()
-    {
-        animation_sequence.Cancel();
-
-        if (isActiveAndEnabled)
-        {
-            state = FeedbackState.Listening;
-        }
     }
 
     internal void PlayReward(
